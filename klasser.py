3f69fb816e7185edde166a400f677d2fd98f9e68
hmauroy <hmauroy@gmail.com>
2026-04-29 10:21:00 +0200

    Utvidet TODO

--- klasser.py
+++ klasser.py
@@ -4,18 +4,25 @@ class Simulering:
     """
     Oppretter Bunndyr og Plankton
     Kjører simuleringsloop med tidsdifferanse.
-    Sjekker kollisjon ved å se på posisjonene til bunndyrene.
+    Sjekker kollisjon ved å se på posisjonene til bunndyr og plankton
+    Tegning gjøres med objektenes tegnemetode.
+    TODO
+    1) Tegne bunndyr
+    2) Tegne plankton
+    3) Animere plankton
+    4) Sjekke kollisjon
+    5) Hva skjer ved kollisjon.
+    6) Lage flere plankton
     """
     def __init__(self, bredde, høyde):
         self.bredde = bredde
-    
 
 class Bunndyr:
     """
     Bunndyr tegnes opp av seg selv, canvas må ligge som en parameter.
     """
-    def __init__(self):
-        pass
+    def __init__(self,canvas):
+        self.canvas = canvas
 
 class Plankton:
     """
@@ -25,5 +32,5 @@ class Plankton:
     ikke giftig -> Grønn farge
     tegnemetode som sjekker giftig for riktig farge. canvas må ligge som parameter.
     """
-    def __init__(self):
-        pass
+    def __init__(self,canvas):
+        self.canvas = canvas
