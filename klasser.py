1202ad9296b4d93d38cd00b25cea6783092f9eba
hmauroy <hmauroy@gmail.com>
2026-04-29 10:34:24 +0200

    Laget bunndyrklasse. Simileringsklasse har window, canvas og kan tegne bunndyret.

--- klasser.py
+++ klasser.py
@@ -1,5 +1,8 @@
 """Klasser for animasjon av et bunndyr som spiser plankton."""
 
+import tkinter as tk
+import time
+
 class Simulering:
     """
     Oppretter Bunndyr og Plankton
@@ -16,13 +19,32 @@ class Simulering:
     """
     def __init__(self, bredde, høyde):
         self.bredde = bredde
+        self.høyde = høyde
+        self.window = tk.Tk()
+        self.window.lift()
+        self.window.focus_force()
+        self.canvas =tk.Canvas(self.window, width=bredde, height=høyde, background="black")
+        self.canvas.pack()
+        self.bunndyr = Bunndyr(self.canvas,self.bredde,self.høyde)
+        self.bunndyr.tegn()
+        self.window.mainloop()
+        
+        
 
 class Bunndyr:
     """
     Bunndyr tegnes opp av seg selv, canvas må ligge som en parameter.
+    Metode for å tegne seg selv.
     """
-    def __init__(self,canvas):
+    def __init__(self,canvas,vindubredde, vinduhøyde):
         self.canvas = canvas
+        self.x = vindubredde/2
+        self.h = 30
+        self.y = vinduhøyde - self.h/2
+        self.w = vindubredde/2
+    
+    def tegn(self):
+        self.canvas.create_rectangle(self.x-self.w/2,self.y-self.h/2, self.x+self.w/2,self.y+self.h/2,fill="grey",tags="bunndyr")
 
 class Plankton:
     """
@@ -34,3 +56,9 @@ class Plankton:
     """
     def __init__(self,canvas):
         self.canvas = canvas
+
+def main():
+    sim = Simulering(600,600)
+
+if __name__ == "__main__":
+    main()
